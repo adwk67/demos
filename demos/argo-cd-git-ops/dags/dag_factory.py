9bb9dbbf1cfdb541fb43b012a8357742737cfb4e
Andrew Kenworthy <1712947+adwk67@users.noreply.github.com>
2025-08-07 10:15:34 +0200

    Update dag_factory.py

--- demos/argo-cd-git-ops/dags/dag_factory.py
+++ demos/argo-cd-git-ops/dags/dag_factory.py
@@ -7,25 +7,25 @@ NUM_DAGS = 1200  # Increase for more stress
 DAG_PREFIX = "stress_dag_"
 
 default_args = {
-'owner': 'airflow',
-'start_date': datetime(2024, 1, 1),
-'retries': 1,
-'retry_delay': timedelta(seconds=5),
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5),
 }
 
 def create_dag(dag_id):
-with DAG(
-    dag_id=dag_id,
-    default_args=default_args,
-    schedule=None,
-    catchup=False,
-    tags=["stress_test"],
-) as dag:
-    start = EmptyOperator(task_id='start')
-    end = EmptyOperator(task_id='end')
-    start >> end
-return dag
+    with DAG(
+        dag_id=dag_id,
+        default_args=default_args,
+        schedule=None,
+        catchup=False,
+        tags=["stress_test"],
+    ) as dag:
+        start = EmptyOperator(task_id='start')
+        end = EmptyOperator(task_id='end')
+        start >> end
+    return dag
 
 for i in range(NUM_DAGS):
-dag_id = f"{DAG_PREFIX}{i:04d}"
-globals()[dag_id] = create_dag(dag_id)
+    dag_id = f"{DAG_PREFIX}{i:04d}"
+    globals()[dag_id] = create_dag(dag_id)
